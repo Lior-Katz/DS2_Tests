a62d6ef12d60f8b08e8b9101d6d9e0179e0ec336
Lior <lior-katz@campus.technion.ac.il>
2024-03-16 21:09:31 +0200

    add utils includes StatusType redefinitions and conversion to string

---
+++ utils.h
@@ -0,0 +1,35 @@
+//
+// Created by Lior on 16/03/2024.
+//
+
+#ifndef DATASTRUCTURES2_UTILS_H
+#define DATASTRUCTURES2_UTILS_H
+
+#include "../wet2util.h"
+
+#define NEW_STATUS_TYPE \
+X(SUCCESS, "Success") \
+X(ALLOCATION_ERROR, "Allocation Error") \
+X(INVALID_INPUT, "Invalid Input") \
+X(FAILURE, "Failure")
+
+#define X(name, str) name,
+enum newStatusType
+{
+	NEW_STATUS_TYPE
+};
+#undef X
+
+#define X(name, str) str,
+constexpr const char* status_type_strings[] = {NEW_STATUS_TYPE};
+#undef X
+
+std::string str(StatusType status)
+{
+	return status_type_strings[static_cast<int>(status)];
+}
+
+
+#define SUCCESS (StatusType::SUCCESS)
+#define FAILURE (StatusType::FAILURE)
+#endif //DATASTRUCTURES2_UTILS_H
